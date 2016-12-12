fea0dcaca7c69d58dbd64ec0a2c53899e626e934
KPS <kps@KPS-Wifi-Mac.local>
2016-12-12 18:38:37 +0100

    working views

--- try2.0/ViewController.swift
+++ try2.0/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var valeur: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	
+	@IBAction func slider(_ sender: UISlider) {
+		valeur.text = String(Int(sender.value))
+	}
+	
 
 
 }
