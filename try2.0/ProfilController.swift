a2f1cd0908b3f3ba8f0d109368945f50f417df8e
Brian <brian@Brians-Mac.local>
2016-12-22 15:57:40 +0100

    2

--- try2.0/ProfilController.swift
+++ try2.0/ProfilController.swift
@@ -67,5 +67,15 @@ class ProfilController: UIViewController {
      }
      }
 
+    
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    
+    }
+ 
+
 
 }
